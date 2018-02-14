13245eabd523967028b95b0af98a6e27bf8cd85b
Zach Johnson <zachtjohnson01@gmail.com>
2018-02-14 07:23:21 -0600

    Sudo code bamazonCustomer.js

---
+++ bamazonCustomer.js
@@ -0,0 +1,43 @@
+// create MySQL database called bamazon
+
+    // create a 'products' table that includes
+
+        // item_id
+
+        // product_name
+
+        // department_name
+
+        // price
+
+        // stock_quantity
+
+    // populate the database with 10 mock products
+
+// require mysql
+
+// require inquirer
+
+// create user interaction
+
+    // display all items available for sale (id, name, price)
+
+    // prompt user:
+
+        // id of product they would like to buy
+
+        // quantity of units they would like to buy
+
+    // verify store has enough quantity of that product to meet customer's request
+
+        // if not, display 'Out of stock', and prevent order completion
+
+        // if so, fulfill customer order
+
+            // update SQL database to reflect remaining quantity
+
+            // once the update goes through, show customer the total cost of their purchase
+
+
+
+
